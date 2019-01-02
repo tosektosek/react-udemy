1a8a5935bf4d4104821aac354431a2f21b72f386
Kamil Toś <tosektosek@gmail.com>
2019-01-02 16:32:17 +0100

    Styling

---
+++ src/containers/App.js
@@ -0,0 +1,86 @@
+import React, { Component } from "react";
+import classes from "./App.css";
+import Persons from "../components/Persons/Persons";
+import Cockpit from "../components/Cockpit/Cockpit";
+
+class App extends Component {
+  state = {
+    persons: [
+      {
+        id: "adadada",
+        name: "Max",
+        age: 22
+      },
+      {
+        id: "fbfcb",
+        name: "Kamil",
+        age: 21
+      },
+      {
+        id: "fgfgf",
+        name: "Sramil",
+        age: 23
+      }
+    ],
+    showPersons: false
+  };
+
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+
+    persons[personIndex] = person;
+    this.setState({
+      persons: persons
+    });
+  };
+
+  deletePersonHandler = personIndex => {
+    // const persons = this.state.persons.slice();
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({ persons: persons });
+  };
+
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({ showPersons: !doesShow });
+  };
+
+  render() {
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        <Persons
+          persons={this.state.persons}
+          clicked={this.deletePersonHandler}
+          changed={this.nameChangedHandler}
+        />
+      );
+    }
+
+    return (
+      <div className={classes.App}>
+        <Cockpit
+          appTitle={this.props.title}
+          clicked={this.togglePersonsHandler}
+          persons={this.state.persons}
+          showPersons={this.state.showPersons}
+        />
+        {persons}
+      </div>
+    );
+  }
+}
+
+export default App;
